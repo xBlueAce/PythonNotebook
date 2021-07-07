98f5dc4239f3e6a94ba8f46767c0d9da673027fb
Carlos Agustin <carlos.agustin218@gmail.com>
2021-07-06 23:51:48 -0700

    adding file

---
+++ Python Objects and data Structure Basics.py
@@ -0,0 +1,13 @@
+
+
+var = 2 + 1
+print (var) #prints out var
+print("==========================\n")
+
+# modulo or "Mod" Operator
+print(7 / 4) # prints out 1.75
+print(7 % 4) # prints out 3
+
+# pow
+print(2 ** 3) # prints out 8
+print( 2 + 10 * 10 + 3) # 105 
